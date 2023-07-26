ecd4f1f0143334d231859d7c67d9f5f5b31b05e4
Long Phan <longnphan@outlook.com>
2023-07-26 00:07:55 -0600

    completed project

--- redux/car-redux/src/components/CarList.jsx
+++ redux/car-redux/src/components/CarList.jsx
@@ -3,15 +3,26 @@ import { removeCar } from "../store";
 
 function CarList() {
   const dispatch = useDispatch();
-  const cars = useSelector(state => state.cars.data);
+  const { cars, name } = useSelector(({ form, cars: { data, searchTerm } }) => {
+    const filteredCars = data.filter(car =>
+      car.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    return {
+      cars: filteredCars,
+      name: form.name,
+    };
+  });
 
   const handleCarDelete = car => {
     dispatch(removeCar(car.id));
   };
 
   const renderedCars = cars.map(car => {
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
+
     return (
-      <div key={car.id} className="panel">
+      <div key={car.id} className={`panel ${bold && "bold"}`}>
         <p>
           {car.name} - ${car.cost}
         </p>
